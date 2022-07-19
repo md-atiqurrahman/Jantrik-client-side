7915b023c6f5d47d6453cd8861cdfa59f49cd127
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-07-19 19:30:18 +0600

    add animation in banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Banner.css';
-import PrimaryButton from '../../Shared/PrimaryButton/PrimaryButton';
 import 'animate.css';
 
 const Banner = () => {
+    const [start, setStart] = useState();
     return (
         <div>
             <div className="carousel w-full">
                 <div id="item1" className="carousel-item w-full">
-                    <div className="hero justify-start lg:min-h-[394px]  bg-[url('/src/images/Banner/HandSaw.png')]  aspect-w-16 aspect-h-9 lg:mt-3 items-start lg:items-center">
+                    <div className="hero justify-start lg:min-h-[394px]  bg-[url('/src/images/Banner/HandSaw.png')] lg:mt-3 items-start lg:items-center">
                         <div className="hero-content justify-start flex-col lg:flex-row-reverse">
                             <div className='lg:ml-10 lg:mr-6'>
-                                <h1 className="banner-title animate__animated animate__bounceInLeft">Big Sale</h1>
-                                <div className='banner-sub-title animate__animated animate__backInRight'>
+                                <h1 className={`banner-title ${start ? 'animate__animated animate__bounceInLeft' : ''}`}>Big Sale</h1>
+                                <div className={`banner-sub-title ${start ? 'animate__animated animate__backInRight' : ''}`}>
                                     <h2>Hands Tools</h2>
                                     <h2>Circular Saw &    Power Saw</h2>
                                 </div>
-                                <PrimaryButton>Shop Now</PrimaryButton>
+                                <button className={`bg-primary  text-white font-normal px-[18px] py-[7px] mt-[31px] rounded ${start ? 'animate__animated animate__bounceInUp' : ''}`}>Shop Now</button>
                             </div>
                         </div>
                     </div>
@@ -25,20 +25,20 @@ const Banner = () => {
                     <div className="hero justify-start lg:min-h-[394px]  bg-[url('/src/images/Banner/DrillMachine.png')]   lg:mt-3 items-start lg:items-center">
                         <div className="hero-content justify-start flex-col lg:flex-row-reverse">
                             <div className='lg:ml-10 lg:mr-6'>
-                                <h1 className="banner-title animate__animated animate__bounceInRight">Big Sale</h1>
-                                <div className='banner-sub-title animate__animated animate__backInRight'>
+                                <h1 className={`banner-title ${!start ? 'animate__animated animate__bounceInLeft' : ''}`}>Big Sale</h1>
+                                <div className={`banner-sub-title ${!start ? ' animate__animated animate__backInRight' : ''}`}>
                                     <h2>Hands Tools</h2>
-                                    <h2>Circular Saw &    Power Saw</h2>
+                                    <h2>Circular Saw & Power Saw</h2>
                                 </div>
-                                <PrimaryButton>Shop Now</PrimaryButton>
+                                <button className={`bg-primary  text-white font-normal px-[18px] py-[7px] mt-[31px] rounded ${!start ? 'animate__animated animate__bounceInUp' : ''}`}>Shop Now</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div >
             <div className="flex justify-center w-full py-2 gap-2 mt-[-30px]">
-                <a href="#item1" className='slider-btn'>1</a>
-                <a href="#item2" className='slider-btn'>2</a>
+                <a onClick={() => setStart(true)} href="#item1" className='slider-btn'>1</a>
+                <a onClick={() => setStart(false)} href="#item2" className='slider-btn'>2</a>
             </div>
         </div>
     );
